0091c6fecef8a7fb9e75894f31b06aabd35b9bcc
TU <agungtriutomo85@gmail.com>
2024-09-06 13:47:39 +0700

    test cek

--- pages/api/expertise.js
+++ pages/api/expertise.js
@@ -20,6 +20,7 @@ const expertise = [
         desc: "GitHub adalah platform pengembangan perangkat lunak berbasis web yang digunakan untuk version control dan collaboration. Di GitHub, developer dapat bekerja bersama-sama dalam sebuah proyek dengan menggunakan Git, sistem version control yang memungkinkan mereka untuk melacak perubahan dalam kode sumber.",
     },
     // {
+        
     //     id: 4,
     //     title: 'Open Source Contributor',
     //     desc: "Open Source is the future. I usually take some time on weekend and contribute into opensource project. It gives me opportunity to learn from best developer's practices and also gives me a chance to help others and contribute into the community for the good.",
